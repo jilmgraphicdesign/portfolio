fbe17306e4b97578faafe5ec80989bc41887f912
JenelIrene <jilmgraphicdesign@gmail.com>
2019-06-10 16:37:11 -0400

    popup java script
    still isn't working yet

---
+++ script.js
@@ -0,0 +1,101 @@
+console.log('hey there')
+
+function showPopup () {
+	var popup = document.getElementByID('popup');
+	popup.classList.add('show')
+}
+
+function hidePopup () {
+	var popup = document.getElementByID('popup');
+	popup.classList.remove('show')
+}
+
+function showPopup () {
+	var popup = document.getElementByID('popup-collection2');
+	popup.classList.add('show')
+}
+
+function hidePopup () {
+	var popup = document.getElementByID('popup-collection2');
+	popup.classList.remove('show')
+}
+
+function showPopup () {
+	var popup = document.getElementByID('popup-earlywork');
+	popup.classList.add('show')
+}
+
+function hidePopup () {
+	var popup = document.getElementByID('popup-earlywork');
+	popup.classList.remove('show')
+}
+
+function showPopup () {
+	var popup = document.getElementByID('popup-illustrations');
+	popup.classList.add('show')
+}
+
+function hidePopup () {
+	var popup = document.getElementByID('popup-illustrations');
+	popup.classList.remove('show')
+}
+
+function showPopup () {
+	var popup = document.getElementByID('popup-nature');
+	popup.classList.add('show')
+}
+
+function hidePopup () {
+	var popup = document.getElementByID('popup-nature');
+	popup.classList.remove('show')
+}
+
+function showPopup () {
+	var popup = document.getElementByID('popup-creative');
+	popup.classList.add('show')
+}
+
+function hidePopup () {
+	var popup = document.getElementByID('popup-creative');
+	popup.classList.remove('show')
+}
+
+function showPopup () {
+	var popup = document.getElementByID('popup-portrait');
+	popup.classList.add('show')
+}
+
+function hidePopup () {
+	var popup = document.getElementByID('popup-portrait');
+	popup.classList.remove('show')
+}
+
+function showPopup () {
+	var popup = document.getElementByID('popup-engagement');
+	popup.classList.add('show')
+}
+
+function hidePopup () {
+	var popup = document.getElementByID('popup-engagement');
+	popup.classList.remove('show')
+}
+
+function showPopup () {
+	var popup = document.getElementByID('popup-logo');
+	popup.classList.add('show')
+}
+
+function hidePopup () {
+	var popup = document.getElementByID('popup-logo');
+	popup.classList.remove('show')
+}
+
+function showPopup () {
+	var popup = document.getElementByID('popup-web');
+	popup.classList.add('show')
+}
+
+function hidePopup () {
+	var popup = document.getElementByID('popup-web');
+	popup.classList.remove('show')
+}
