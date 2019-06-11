fcfaac064abbf9c30e40931d8b71fa07ad3289a2
JenelIrene <jilmgraphicdesign@gmail.com>
2019-06-11 13:34:54 -0400

    POPUPS ARE FIXED YAY!

--- script.js
+++ script.js
@@ -1,12 +1,101 @@
 
 
-function showPopup () {
-	var popup = document.getElementById('popup');
+function showPopupc1 () {
+	var popup = document.getElementById('popup-collection1');
 	popup.classList.add('show')
 }
 
-function hidePopup () {
-	var popup = document.getElementById('popup');
+function hidePopupc1 () {
+	var popup = document.getElementById('popup-collection1');
 	popup.classList.remove('show')
 }
 
+function showPopupc2 () {
+	var popup = document.getElementById('popup-collection2');
+	popup.classList.add('show')
+}
+
+function hidePopupc2 () {
+	var popup = document.getElementById('popup-collection2');
+	popup.classList.remove('show')
+}
+
+function showPopupew () {
+	var popup = document.getElementById('popup-earlywork');
+	popup.classList.add('show')
+}
+
+function hidePopupew () {
+	var popup = document.getElementById('popup-earlywork');
+	popup.classList.remove('show')
+}
+
+function showPopupil () {
+	var popup = document.getElementById('popup-illustrations');
+	popup.classList.add('show')
+}
+
+function hidePopupil () {
+	var popup = document.getElementById('popup-illustrations');
+	popup.classList.remove('show')
+}
+
+function showPopupnp () {
+	var popup = document.getElementById('popup-nature');
+	popup.classList.add('show')
+}
+
+function hidePopupnp () {
+	var popup = document.getElementById('popup-nature');
+	popup.classList.remove('show')
+}
+
+function showPopupcp () {
+	var popup = document.getElementById('popup-creative');
+	popup.classList.add('show')
+}
+
+function hidePopupcp () {
+	var popup = document.getElementById('popup-creative');
+	popup.classList.remove('show')
+}
+
+function showPopuppo () {
+	var popup = document.getElementById('popup-portrait');
+	popup.classList.add('show')
+}
+
+function hidePopuppo () {
+	var popup = document.getElementById('popup-portrait');
+	popup.classList.remove('show')
+}
+
+function showPopupep () {
+	var popup = document.getElementById('popup-engagement');
+	popup.classList.add('show')
+}
+
+function hidePopupep () {
+	var popup = document.getElementById('popup-engagement');
+	popup.classList.remove('show')
+}
+
+function showPopupld () {
+	var popup = document.getElementById('popup-logo');
+	popup.classList.add('show')
+}
+
+function hidePopupld () {
+	var popup = document.getElementById('popup-logo');
+	popup.classList.remove('show')
+}
+
+function showPopupwd () {
+	var popup = document.getElementById('popup-web');
+	popup.classList.add('show')
+}
+
+function hidePopupwd () {
+	var popup = document.getElementById('popup-web');
+	popup.classList.remove('show')
+}
