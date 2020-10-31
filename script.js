61a8f5ffd6c8c47f45cd731739cbf5c6a0fd1193
JenelIrene <jilmgraphicdesign@gmail.com>
2020-10-30 23:24:58 -0400

    gallery start

--- script.js
+++ script.js
@@ -1,101 +1,13 @@
 
-
-function showPopupc1 () {
-	var popup = document.getElementById('popup-collection1');
-	popup.classList.add('show')
-}
-
-function hidePopupc1 () {
-	var popup = document.getElementById('popup-collection1');
-	popup.classList.remove('show')
-}
-
-function showPopupc2 () {
-	var popup = document.getElementById('popup-collection2');
-	popup.classList.add('show')
-}
-
-function hidePopupc2 () {
-	var popup = document.getElementById('popup-collection2');
-	popup.classList.remove('show')
-}
-
-function showPopupew () {
-	var popup = document.getElementById('popup-earlywork');
-	popup.classList.add('show')
-}
-
-function hidePopupew () {
-	var popup = document.getElementById('popup-earlywork');
-	popup.classList.remove('show')
-}
-
-function showPopupil () {
-	var popup = document.getElementById('popup-illustrations');
-	popup.classList.add('show')
-}
-
-function hidePopupil () {
-	var popup = document.getElementById('popup-illustrations');
-	popup.classList.remove('show')
-}
-
-function showPopupnp () {
-	var popup = document.getElementById('popup-nature');
-	popup.classList.add('show')
-}
-
-function hidePopupnp () {
-	var popup = document.getElementById('popup-nature');
-	popup.classList.remove('show')
-}
-
-function showPopupcp () {
-	var popup = document.getElementById('popup-creative');
-	popup.classList.add('show')
-}
-
-function hidePopupcp () {
-	var popup = document.getElementById('popup-creative');
-	popup.classList.remove('show')
-}
-
-function showPopuppo () {
-	var popup = document.getElementById('popup-portrait');
-	popup.classList.add('show')
-}
-
-function hidePopuppo () {
-	var popup = document.getElementById('popup-portrait');
-	popup.classList.remove('show')
-}
-
-function showPopupep () {
-	var popup = document.getElementById('popup-engagement');
-	popup.classList.add('show')
-}
-
-function hidePopupep () {
-	var popup = document.getElementById('popup-engagement');
-	popup.classList.remove('show')
-}
-
-function showPopupld () {
-	var popup = document.getElementById('popup-logo');
-	popup.classList.add('show')
-}
-
-function hidePopupld () {
-	var popup = document.getElementById('popup-logo');
-	popup.classList.remove('show')
-}
-
-function showPopupwd () {
-	var popup = document.getElementById('popup-web');
-	popup.classList.add('show')
-}
-
-function hidePopupwd () {
-	var popup = document.getElementById('popup-web');
-	popup.classList.remove('show')
+function myFunction(imgs) {
+  // Get the expanded image
+  var expandImg = document.getElementById("expandedImg");
+  // Get the image text
+  var imgText = document.getElementById("imgtext");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
 }
